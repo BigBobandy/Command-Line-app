69e545f82025553b0133758a34d249984446a023
BigBobandy <jdykes207@gmail.com>
2023-05-02 11:57:50 -0400

    moved things in the folder and added comments
    Added comments to the code in app.jsx and aslo created the folder for the backend

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import "./App.css";
 function App() {
   // Declaring state variables for input and output
   const [command, setCommand] = useState("");
+  // Output state variable is an array which will contain the lines of the command output
   const [output, setOutput] = useState([]);
 
   // Handle changes to the command input field
   const handleCommandChange = (event) => {
+    // Update the command variable with the new input
     setCommand(event.target.value);
   };
 
+  // Handle form submission that so that the command is executed
   const handleCommandSubmit = (event) => {
+    // Prevent the page from reloading on submit
     event.preventDefault();
   };
 
@@ -22,8 +26,8 @@ function App() {
         <input
           type="text"
           placeholder="Enter command"
-          value={command}
-          onChange={handleCommandChange}
+          value={command} // Binding input field to the command state variable
+          onChange={handleCommandChange} // Update command state variable when input changes
         />
         <button type="submit">Execute</button>
       </form>
