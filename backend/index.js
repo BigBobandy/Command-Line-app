42d78d5a15b69f3c052db9377af1e3d8a005bfa0
BigBobandy <jdykes207@gmail.com>
2023-05-02 13:37:03 -0400

    Set up a basic Express server with CORS and body-parser middleware
    created a basic Express server and added CORS and body-parser middleware. The CORS middleware enables Cross-Origin Resource Sharing, allowing our frontend application to communicate with the backend server. The body-parser middleware is responsible for parsing incoming JSON payloads in the request body, making it easier for our server-side code to work with the data sent by the client. Also configured the server to listen on a specified port (either from an environment variable or the default port 3001).

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+// Importing the required modules
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+// Create a new express application
+const app = express();
+
+// Middleware
+// Enable CORS (Cross-Origin Resource Sharing) for the server
+app.use(cors());
+// Use the bodyParser middleware to parse JSON payloads in incoming requests
+app.use(bodyParser.json());
+
+// Set the server's listening port, using the enviroments variable PORT if available, or defualting to 3001
+const PORT = process.env.PORT || 3001;
+
+// Start the server and listen for incoming requests on the specified port
+app.listen(PORT, () => {
+  // Log a message to the console to indicate that the server has started successfully
+  console.log("Server is running on port ${PORT}");
+});
+
+const commandsRoute = require("./routes/commands");
+
+app.use("/api", commandsRoute);
